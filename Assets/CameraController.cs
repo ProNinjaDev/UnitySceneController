bdcec04bcc5d0ab566b192c22506d3d6643a1cdf
ProNinjaDev <pronin-04@bk.ru>
2025-06-30 00:44:47 +0300

    Implement rotation around objects

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -6,7 +6,8 @@ public class CameraController : MonoBehaviour
 {
     private Transform currentTarget;
     public Vector3 offset = new Vector3(0f, 3f, -5f);
-    public float smoothSpeed = 5f;
+    public float smoothSpeed = 3f;
+    public float rotationSpeed = 5f;
 
     void Start()
     {
@@ -22,6 +23,21 @@ public class CameraController : MonoBehaviour
     {
         if (currentTarget != null) 
         {
+            if (Input.GetMouseButton(1))
+            {
+                float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
+                offset = Quaternion.AngleAxis(mouseX, Vector3.up) * offset;
+                float mouseY = -Input.GetAxis("Mouse Y") * rotationSpeed;
+                Vector3 oldOffset = offset;
+                offset = Quaternion.AngleAxis(mouseY, transform.right) * offset;
+
+                float angle = Vector3.Angle(Vector3.up, offset);
+                if (angle < 5f || angle > 85f)
+                {
+                    offset = oldOffset;
+                }
+            }
+
             Vector3 targetCameraPos = currentTarget.position + offset;
             Vector3 newPosition = Vector3.Lerp(transform.position, targetCameraPos, smoothSpeed * Time.deltaTime);
             transform.position = newPosition;
