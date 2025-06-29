da84100fd62acdbbaf8c65b12464d31a3b2f854b
ProNinjaDev <pronin-04@bk.ru>
2025-06-29 19:55:07 +0300

    Add an interactive object selection script

---
+++ Assets/SelectionHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionHandler : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log($"Selected object: {hit.collider.gameObject.name}");
+            }
+        }
+    }
+}
