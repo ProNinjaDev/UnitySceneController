f046c159fddc72a1871239b39f95dc220c3d9bd6
ProNinjaDev <pronin-04@bk.ru>
2025-06-30 15:26:29 +0300

    Create a script that displays interactive objects in the interface

--- Assets/UIListController.cs
+++ Assets/UIListController.cs
@@ -5,9 +5,29 @@ using TMPro;
 
 public class UIListController : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject lineOfListPrefab;
+
+    [SerializeField]
+    private Transform listContainer;
+
     void Start()
     {
-        
+        GameObject[] interactableObjects = GameObject.FindGameObjectsWithTag("Interactable");
+
+        foreach (var obj in interactableObjects)
+        {
+            GameObject newLine = Instantiate(lineOfListPrefab);
+
+            newLine.transform.SetParent(listContainer, false);
+
+            TextMeshProUGUI objectNameText = newLine.GetComponentInChildren<TextMeshProUGUI>();
+
+            if (objectNameText != null)
+            {
+                objectNameText.text = obj.name;
+            }
+        }
     }
 
     void Update()
